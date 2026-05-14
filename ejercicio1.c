85e39b5b3b4151f8301d02f4416c93b3c337b6f1
Angie Arias <angiearias32@gmail.com>
2026-05-13 23:31:09 -0600

    correccion de  error en funcion de potencia

---
+++ ejercicio1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int potencia(int base, int exp) {
+    int resultado = 1;
+
+    while (exp > 0) {
+        resultado = resultado * base;
+        exp--;
+    }
+
+    return resultado;
+
+}
+
+int main(void) {
+
+    printf("2^8 = %d\n", potencia(2, 8));
+    printf("3^4 = %d\n", potencia(3, 4));
+
+    return 0;
+
+
+} 
