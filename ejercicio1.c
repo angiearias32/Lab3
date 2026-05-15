7399a0d321a5139f2d5dcd3fb9d3c69aaaa8c1e3
Angie Arias <angiearias32@gmail.com>
2026-05-14 20:29:45 -0600

    Finalizadas las 4 partes del ejercicio 1

--- ejercicio1.c
+++ ejercicio1.c
@@ -3,6 +3,7 @@
 int potencia(int base, int exp) {
     int resultado = 1;
 
+
     while (exp > 0) {
         resultado = resultado * base;
         exp--;
@@ -12,12 +13,54 @@ int potencia(int base, int exp) {
 
 }
 
+int es_par(int n) {
+    if (n % 2 == 0) {
+        return 1;
+    } else {
+        return 0;
+
+    }
+}
+
 int main(void) {
 
-    printf("2^8 = %d\n", potencia(2, 8));
-    printf("3^4 = %d\n", potencia(3, 4));
+    int base; // En esta variable se guarda la base
+    int exp; // Variable para guardar exponente
+    int resultado; // 
+
+    do {
+       printf("Ingresar base distinta de 0: ");
+       scanf("%d", &base); //pide una base al ususario, %d significa entero 
+
+       if (base == 0) {  // revisa si la base no es valida
+           printf("error: base no puede ser 0.\n");
+       } // muestra un error si se ingresa 0
+
+    } while (base == 0); // se repte hasta que se ingrese una base valida
+
+    do {
+        printf("Ingresar un exponente positivo: ");
+        scanf("%d", &exp);
+
+        if (exp < 0) {   // muestra error cuando se ingresa exp negativo
+            printf("error: exponente no puede ser negativo.\n");
+
+        }
+
+    } while (exp < 0);
+
+    resultado = potencia(base, exp);
+
+    printf("resultado: %d\n", resultado);
+
+    if (es_par(resultado)) {
+        printf("Resultado es par.\n");
+
+    } else {
+        printf("resultado es impar.\n");
+
+} 
 
     return 0;
-
 
 } 
